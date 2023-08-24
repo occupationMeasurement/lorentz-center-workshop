bb08d0004cf3170630f8c8cf511d54c4ed9a10f9
Jan Simson <git@simson.io>
2023-08-24 09:41:01 +0200

    WIP xgboost support

--- workshop/modelling.py
+++ workshop/modelling.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from xgboost import XGBClassifier
 
 from .config import default_config
 
@@ -8,7 +9,10 @@ def train_model(X_train: pd.DataFrame, Y_train: pd.Series, config: dict = defaul
     if (config["model"] == "gradient_boosting"):
         model = GradientBoostingClassifier()
         model.fit(X_train, Y_train)
-    if (config["model"] == "knn"):
+    elif (config["model"] == "xgboost"):
+        model = XGBClassifier(objective='multi:softprob') # , num_class=Y_train.unique().size
+        model.fit(X_train, Y_train)
+    elif (config["model"] == "knn"):
         model = KNeighborsClassifier()
         model.fit(X_train, Y_train)
     else:
@@ -17,7 +21,7 @@ def train_model(X_train: pd.DataFrame, Y_train: pd.Series, config: dict = defaul
     return model
 
 def predict(model, X_test: pd.DataFrame, config: dict = default_config) -> pd.Series:
-    if (config["model"] == "gradient_boosting" or config["model"] == "knn"):
+    if (config["model"] == "gradient_boosting" or config["model"] == "knn" or config["model"] == "xgboost"):
         y_pred = model.predict(X_test)
     else:
         raise ValueError("Unknown model type")
